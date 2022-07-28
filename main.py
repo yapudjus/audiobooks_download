e28abb3807f59658e423e7a45b322574a406afac
yapudjus <contact@mail.yapudjusowndomain.fr>
2022-07-28 18:55:53 +0200

    added debug output, some file organisation stuff

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from src import litteratureaudio
 from src import common
 import html
 
+debug = True
+
+debugger = common.debug(debug)
+
 query = input("Search: ")
 
 print(f'searching for "{query}" on galaxyaudiobook.com')
@@ -68,50 +72,72 @@ bookcount = 0
 for book in to_down:
     bookcount += 1
     book["title"] = html.unescape(book["title"])
+    debugger.log(f"{bookcount}/{len(to_down)} {book['title']}")
     bookdir = os.path.join(
         os.getcwd(), "out", re.sub(r'( |[/:,.;!§*µ$£¨ù%&"])', "_", book["title"])
     )
     if not os.path.isdir(bookdir):
         os.mkdir(bookdir)
-    coverfile = os.path.join(bookdir, "cover.jpg")
-    print(f'Downloading cover for {book["title"]}')
-    common.common.download_file(book["image"], coverfile)
-    common.common.write_data(book, os.path.join(bookdir, "book.json"))
+    if not os.path.isfile(os.path.join(bookdir, "loaded")):
+        coverfile = os.path.join(bookdir, "cover.jpg")
+        print(f'Downloading cover for {book["title"]}')
+        debugger.log(f'{book["image"]} => {coverfile}')
+        common.common.download_file(book["image"], coverfile, lbl=book["image"])
+        debugger.log(f'{book} => {os.path.join(bookdir, "book.json")}')
+        common.common.write_data(book, os.path.join(bookdir, "book.json"))
 
-    if sourcetype == "Galaxy":
-        tracks = galaxyaudiobooks.download(book["url"]).tracks
-    if sourcetype == "Bookaudio":
-        tracks = bookaudiobooks.download(book["url"]).tracks
-    if sourcetype == "litteratureaudio":
-        tracks = litteratureaudio.download(book["url"]).tracks
-    trackcount = 0
-    for track in tracks:
-        trackcount += 1
-        track["name"] = html.unescape(track["name"])
-        print(
-            f'Downloading track {track["name"]} ({trackcount}/{len(tracks)}) from book {book["title"]} ({bookcount}/{len(to_down)}) '
-        )
-        out = (
-            os.path.join(bookdir, re.sub(r'( |[/:,.;!§*µ$£¨ù%&"])', "_", track["name"]))
-            + ".mp3"
-        )
         if sourcetype == "Galaxy":
-            link = galaxyaudiobooks.download.get_url(track["chapter_id"])
-        elif sourcetype == "Bookaudio":
-            link = track["url"]
-        elif sourcetype == "litteratureaudio":
-            link = track["url"]
-            out = out.replace(".mp3", ".zip")
-        outpath = common.common.download_file(link, out)
-        print(f"Saved to {outpath}")
-        if sourcetype == "litteratureaudio" :
-            # unzip the file to the same folder
-            import zipfile
-            print(f"Unzipping {outpath} to {bookdir}")
-            zip_file = outpath
-            try:
-                with zipfile.ZipFile(zip_file) as z:
-                    z.extractall(bookdir)
-                    print("Extracted all")
-            except:
-                print("Invalid file")
+            tracks = galaxyaudiobooks.download(book["url"]).tracks
+        if sourcetype == "Bookaudio":
+            tracks = bookaudiobooks.download(book["url"]).tracks
+        if sourcetype == "litteratureaudio":
+            tracks = litteratureaudio.download(book["url"]).tracks
+        trackcount = 0
+        for track in tracks:
+            trackcount += 1
+            track["name"] = html.unescape(track["name"])
+            print(
+                f'Downloading track {track["name"]} ({trackcount}/{len(tracks)}) from book {book["title"]} ({bookcount}/{len(to_down)}) '
+            )
+            out = (
+                os.path.join(bookdir, re.sub(r'( |[/:,.;!§*µ$£¨ù%&"])', "_", track["name"]))
+                + ".mp3"
+            )
+            if sourcetype == "Galaxy":
+                link = galaxyaudiobooks.download.get_url(track["chapter_id"])
+            elif sourcetype == "Bookaudio":
+                link = track["url"]
+            elif sourcetype == "litteratureaudio":
+                link = track["url"]
+                out = out.replace(".mp3", ".zip")
+            debugger.log(f'{link} => {out}')
+            outpath = common.common.download_file(link, out, lbl = f'{book["title"]} ({bookcount}/{len(to_down)})')
+            print(f"Saved to {outpath}")
+            
+            if sourcetype == "litteratureaudio" :
+                # unzip the file to the same folder
+                import zipfile
+                debugger.log(f"Unzipping {outpath} to {bookdir}")
+                zip_file = outpath
+                try:
+                    with zipfile.ZipFile(zip_file) as z:
+                        z.extractall(bookdir)
+                        print("Extracted all")
+                except:
+                    print("Invalid file")
+    with open(os.path.join(bookdir, "loaded"), "w") as f:
+        f.write("loaded")
+    if sourcetype == "litteratureaudio":
+        for i in os.listdir(bookdir):
+            if i.endswith(".zip"):
+                print(f'removing file {i}')
+                os.remove(os.path.join(bookdir, i))
+    for i in os.listdir(bookdir):
+        if os.path.isdir(os.path.join(bookdir, i)):
+            for j in os.listdir(os.path.join(bookdir, i)):
+                if j.endswith(".mp3"):
+                    debugger.log(f'{os.path.join(bookdir, i, j)} => {os.path.join(bookdir)}')
+                    os.rename(os.path.join(bookdir, i, j), os.path.join(bookdir, j))
+                elif j.endswith(".txt") :
+                    os.remove(os.path.join(bookdir, i, j))
+            os.rmdir(os.path.join(bookdir, i))
--- src/common.py
+++ src/common.py
@@ -9,11 +9,15 @@ class common :
         with open(path, "w") as f:
             f.write(json.dumps(data))
     
-    def download_file(url, out):
+    def download_file(url, out, lbl = ""):
         url = urllib.parse.unquote(url)
-        r = requests.get(url, stream=True)
-        total_length = int(r.headers.get("content-length"))
         dodownload = True
+        r = requests.get(url, stream=True)
+        try :
+            total_length = int(r.headers.get("content-length"))
+        except :
+            print(f'{url} : No content-length header')
+            dodownload = False
         if os.path.exists(out):
             if os.path.getsize(out) == total_length:
                 dodownload = False
@@ -21,10 +25,22 @@ class common :
                 os.remove(out)
         if dodownload:
             with open(out, "wb") as f:
-                for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+                for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1, label=lbl, filled_char="█", width=50):
                     if chunk:
                         f.write(chunk)
                         f.flush()
         else :
             print("File already exists")
         return out
+
+class debug :
+    def __init__(self, debug = False):
+        self.debug = debug
+        self.debug_print = print if self.debug else lambda *a, **k: None
+        self.debug_print("Debug mode is on")
+    def __call__(self, *a, **k):
+        self.debug_print(*a, **k)
+    def log(self, *a, **k):
+        self.debug_print(*a, **k)
+    def debug_print(self, *a, **k):
+        print(*a, **k)
